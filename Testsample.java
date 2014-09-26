b13370aa3a81b8e53dd58eefee03d67903deb361
admin <pkumawat@chambal.com>
2014-09-26 04:56:56 -0700

    first commit

---
+++ Testsample.java
@@ -0,0 +1,16 @@
+
+public class Testsample {
+	
+		public static void main(String[] args)
+		{
+		int input = 1;
+		int finalResult = summation(input);
+		}
+	public static int summation(int n)
+	{
+	int result = (n * (n*1))/2;
+	return result;
+	}
+	}
+
+
