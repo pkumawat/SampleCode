2d9a39e6084c68ca4a428a7b9e0ea845687bf621
admin <pkumawat@chambal.com>
2014-09-26 05:16:13 -0700

    second file

---
+++ Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+	public static void main(String[] args)
+	{
+	int input = 11;
+	int finalResult = summation(input);
+	}
+public static int summation(int n)
+{
+int result = (n * (n+1))/2;
+return result;
+}
+}
