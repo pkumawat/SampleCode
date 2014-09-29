8523f3c3c54715bf039ed71ad189de797e955eb3
admin <pkumawat@chambal.com>
2014-09-28 23:27:23 -0700

    adding third  file on monday

---
+++ subtraction.java
@@ -0,0 +1,15 @@
+
+public class subtraction {
+	public static void main(String[] args)
+	{
+	int input = 151;
+	int finalResult = summation(input);
+	}
+public static int summation(int n)
+{
+int result = (n * (n-1))/2;
+return result;
+}
+}
+
+
