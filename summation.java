576c8e9b3beb5a2d3c8fddedeecc702a266ffa88
admin <pkumawat@chambal.com>
2014-09-28 22:08:06 -0700

    adding file on monday

---
+++ summation.java
@@ -0,0 +1,14 @@
+
+public class summation {
+		public static void main(String[] args)
+		{
+		int input = 155;
+		int finalResult = summation(input);
+		}
+	public static int summation(int n)
+	{
+	int result = (n * (n+1))/2;
+	return result;
+	}
+	}
+
