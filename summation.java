c1eb8336e4ce65f1268ffadd2c6899611e3ce236
admin <pkumawat@chambal.com>
2014-09-28 22:49:14 -0700

    adding second file on monday

--- summation.java
+++ summation.java
@@ -2,7 +2,7 @@
 public class summation {
 		public static void main(String[] args)
 		{
-		int input = 155;
+		int input = 150;
 		int finalResult = summation(input);
 		}
 	public static int summation(int n)
