a4d60da0c25ff3e49d1156c42121c29f48d7d8ad
pkumawat <pkumawat@chambal.com>
2014-09-30 18:32:11 +0530

    p

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+public class Demo {
+	public static void main(String[] args)
+	{
+	int input = 152;
+	int finalResult = summation(input);
+	}
+public static int summation(int n)
+{
+int result = (n * (n/1))/2;
+return result;
+}
+}
